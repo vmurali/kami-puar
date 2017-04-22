70bf0494a4cb36b2a8d67fd41d960c513ecab476
Joonwon Choi <joonwonc@mit.edu>
2017-04-22 11:57:47 -0400

    Deterministic semantics, almost done.

--- refinements/StepDet.v
+++ refinements/StepDet.v
@@ -84,12 +84,35 @@ Section NoCalls.
     intros; eapply actionNoCalls_SemAction; eauto.
   Qed.
 
+  Corollary dmNoCalls_SubstepsInd:
+    forall m (Hequiv: ModEquiv type typeUT m)
+           (Hdms: Forall (fun dm => dmNoCalls dm = true) (getDefsBodies m))
+           o u l,
+      SubstepsInd m o u l ->
+      forall ds cs,
+        l = {| annot := None; defs := ds; calls := cs |} ->
+        cs = M.empty _.
+  Proof.
+    induction 3; simpl; intros; subst; [inv H; reflexivity|].
+
+    destruct l as [pann pds pcs]; simpl in *; inv H4.
+    destruct sul as [|]; destruct pann; try discriminate.
+    specialize (IHSubstepsInd _ _ eq_refl); subst.
+
+    inv H0; auto.
+    apply dmNoCalls_Substep in HAction; subst; auto.
+    - unfold ModEquiv in Hequiv; destruct Hequiv.
+      eapply MethsEquiv_in in HIn; eauto.
+    - eapply Forall_forall in Hdms; eauto.
+  Qed.
+
 End NoCalls.
 
 Section OneDepth.
   Variable m : Modules.
   Hypotheses (Hequiv: ModEquiv type typeUT m)
-             (Hdms: Forall (fun dm => dmNoCalls dm = true) (getDefsBodies m)).
+             (Hdms: Forall (fun dm => dmNoCalls dm = true) (getDefsBodies m))
+             (Hedms: getExtDefs m = nil).
 
   Section GivenOldRegs.
     Variable o : RegsT.
@@ -123,17 +146,7 @@ Section OneDepth.
               cs = M.complement rcs (getDefs m) ->
               StepDet u {| annot := Some (Some rn);
                            defs := M.empty _;
-                           calls := cs |}
-    | SbMeth:
-        forall mu mcs mn mar,
-          (* mcs should be empty, but we don't care. *)
-          Substep m o mu (Meth (Some (mn :: mar)%struct)) mcs ->
-          In mn (getExtDefs m) ->
-          forall ds,
-            ds = [mn <- mar]%fmap ->
-            StepDet mu {| annot := None;
-                          defs := ds;
-                          calls := mcs |}.
+                           calls := cs |}.
 
   End GivenOldRegs.
 
@@ -234,43 +247,12 @@ Section OneDepth.
             rewrite M.complement_find.
             destruct (in_dec _ k (getDefs m)); auto.
             elim n; auto.
-
-      - inv H0; inv Hsig.
-        eapply Forall_forall in Hdms; eauto.
-        assert (Hf: MethEquiv type typeUT f).
-        { unfold ModEquiv in Hequiv; destruct Hequiv.
-          eapply MethsEquiv_in in H0; eauto.
-        }
-        pose proof (dmNoCalls_Substep Hf Hdms _ HAction); subst.
-        match goal with
-        | [ |- Step _ _ _ ?l ] => change l with (hide l) 
-        end.
-        apply step_consistent.
-        constructor.
-        + econstructor 2 with
-          (su:= u) (sul:= Meth (Some (attrName f :: _)%struct)) (scs:= M.empty _).
-          * constructor.
-          * econstructor; eauto.
-          * unfold CanCombineUUL; cbn; repeat split; auto.
-            findeq.
-          * reflexivity.
-          * reflexivity.
-        + unfold wellHidden; cbn; split.
-          * unfold M.KeysDisj; intros.
-            findeq.
-            unfold getExtDefs in H1.
-            apply filter_In in H1; dest.
-            apply negb_true_iff in H1.
-            apply eq_sym, string_in_dec_not_in in H1; elim H1; auto.
-          * apply M.KeysDisj_empty.
     Qed.
 
   End FromDet.
 
   (** * [Step] implies [StepDet] *)
   Section ToDet.
-    (* Let's just make it easier with one more practical assumption. *)
-    Hypothesis (Hedms: getExtDefs m = nil).
 
     Lemma getExtDefs_nil_step_ds:
       forall o u a ds cs,
@@ -383,6 +365,62 @@ Section OneDepth.
       eapply substepsInd_update_empty; eauto.
     Qed.
 
+    Lemma substepMeths_pull_hd:
+      forall o ms1 ms2 m u,
+        SubstepMeths o (m :: (ms1 ++ ms2)) u ->
+        SubstepMeths o (ms1 ++ m :: ms2) u.
+    Proof.
+      induction ms1; simpl; intros; auto.
+      inv H.
+      inv H2.
+      assert (SubstepMeths o ((mn, mar) :: (ms1 ++ ms2)) (M.union pu0 u0))
+        by (econstructor; eauto).
+      specialize (IHms1 _ _ _ H).
+      replace (M.union (M.union pu0 u) u0) with (M.union (M.union pu0 u0) u) by meq.
+      econstructor; eauto.
+    Qed.
+    
+    Lemma substepsInd_implies_substepMeths:
+      forall o u l,
+        SubstepsInd m o u l ->
+        forall ds,
+          l = {| annot := None; defs := ds; calls := M.empty _ |} ->
+          SubstepMeths o (M.elements ds) u.
+    Proof.
+      induction 1; simpl; intros; subst; [inv H; constructor|].
+
+      destruct l as [pann pds pcs]; simpl in *.
+      destruct pann; [inv H4; destruct sul; discriminate|].
+      destruct sul as [|om]; [inv H4|].
+
+      eapply dmNoCalls_SubstepsInd in H; eauto; subst.
+      specialize (IHSubstepsInd _ eq_refl).
+
+      destruct om as [[mn mar]|]; [|inv H4; inv H0; mred].
+      inv H4; mred; subst.
+
+      replace (M.union (M.add mn mar (M.empty _)) pds)
+      with (M.add mn mar pds) by meq.
+
+      assert (SubstepMeths o ((mn, mar) :: (M.elements pds)) (M.union u su)).
+      { econstructor; eauto.
+        inv H1; auto.
+      }
+
+      assert (M.F.P.Add mn mar pds (M.add mn mar pds)) by (unfold M.F.P.Add; auto).
+      apply M.F.elements_Add in H2; [|inv H1; simpl in *; dest; auto].
+
+      replace (M.Map.elements (M.add mn mar pds))
+      with ((M.F.elements_lt (mn, mar) pds)
+              ++ (mn, mar) :: M.F.elements_ge (mn, mar) pds).
+      - apply substepMeths_pull_hd.
+        rewrite <-M.F.elements_split; auto.
+      - apply eq_sym, M.eq_leibniz_list.
+        clear -H2.
+
+        admit. (* where is it? *)
+    Admitted.
+
     Theorem step_implies_stepDet:
       forall o u l ,
         Step m o u l ->
@@ -398,6 +436,12 @@ Section OneDepth.
       destruct orn as [rn|];
         [|pose proof (getExtDefs_nil_step_empty H (or_introl eq_refl)); dest; subst;
           econstructor].
+
+      assert (M.restrict cs (getDefs m) = M.empty _).
+      { pose proof (step_calls_extCalls_in Hequiv H); simpl in H0.
+        eapply M.restrict_DisjList; eauto.
+        apply extCalls_defs_disj.
+      }
         
       remember {| annot := Some (Some rn); defs := M.empty _; calls := cs |}.
       apply step_consistent in H.
@@ -406,21 +450,46 @@ Section OneDepth.
       assert (exists ms,
                  l0 = {| annot := Some (Some rn);
                          defs := ms;
-                         calls := M.union ms cs |}) by admit.
+                         calls := M.union ms cs |}).
+      { clear -H2.
+        destruct l0 as [ann0 ds0 cs0]; unfold hide in H2; simpl in *.
+        inv H2.
+        exists ds0; f_equal.
+
+        M.ext y.
+        apply M.Equal_val with (k:= y) in H1.
+        rewrite M.subtractKV_find in H1.
+        rewrite M.find_union, M.subtractKV_find.
+        destruct (M.find y ds0), (M.find y cs0); auto.
+        destruct (signIsEq s s0); subst; auto.
+        inv H1.
+      }
+      
       dest; subst.
-      rewrite H1; clear H1.
+      rewrite H2; clear H2 HWellHidden.
       apply substepsInd_rule_split with (or := Some rn) in HSubSteps; [|subst; reflexivity].
       dest; subst.
+
+      destruct x3 as [mann mds mcs]; simpl in *.
+      destruct mann; [inv H2; dest; inv H5|].
+      pose proof (dmNoCalls_SubstepsInd Hequiv Hdms H1 eq_refl); subst; mred.
+      inv H4.
 
       eapply SbRule.
       - eassumption.
       - instantiate (1:= x2).
-        admit.
-      - inv H1; auto.
-      - inv H1; apply M.union_comm; auto.
-      - admit.
-
-    Admitted.
+        pose proof (substepsInd_defs_in H1); simpl in H3.
+        rewrite M.restrict_union; rewrite H0; mred.
+        rewrite M.restrict_KeysSubset by assumption.
+        eapply substepsInd_implies_substepMeths; eauto.
+      - inv H2; auto.
+      - inv H2; apply M.union_comm; auto.
+      - rewrite M.complement_union.
+        pose proof (substepsInd_defs_in H1); simpl in H3.
+        rewrite M.complement_KeysSubset by assumption; mred.
+        rewrite M.restrict_complement_union with (d:= getDefs m) at 1.
+        rewrite H0; mred.
+    Qed.
 
   End ToDet.
 
