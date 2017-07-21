d1cfecd9000411089a3b0d175f629852245bd433
Joonwon Choi <joonwonc@mit.edu>
2017-07-21 19:38:19 -0400

    Defined a non-restoring divider and its spec

--- Puar/MulDiv.v
+++ Puar/MulDiv.v
@@ -3,26 +3,26 @@ Require Import Lib.Indexer Lib.Struct Lib.FMap Lib.Reflection.
 Require Import Kami.Tactics Kami.SemFacts.
 Require Import Puar.Useful Puar.Processor FunctionalExtensionality.
 
-(* Below implements a radix-4 Booth multiplier. *)
+(* Below implements a radix-4 Booth Multiplier. *)
 Section Multiplier64.
   (* NOTE: hard to declare [LogNumPhases] and [NumBitsPerPhase] as variables,
    * since truncation and extension require certain equation w.r.t. bit-lengths.
    *)
-  (* Variable LogNumPhases NumBitsPerPhase: nat. *)
-  Definition LogNumPhases := 3.
-  Definition NumBitsPerPhase := 8.
+  (* Variable MultLogNumPhases MultNumBitsPerPhase: nat. *)
+  Definition MultLogNumPhases := 3.
+  Definition MultNumBitsPerPhase := 8.
 
-  Local Definition NumPhases := wordToNat (wones LogNumPhases) + 1.
-  Local Definition NumBits := NumPhases * NumBitsPerPhase.
-  Local Definition MultBits := 2 * NumBits + 2.
+  Local Definition MultNumPhases := wordToNat (wones MultLogNumPhases) + 1.
+  Local Definition MultNumBits := MultNumPhases * MultNumBitsPerPhase.
+  Local Definition MultBits := 2 * MultNumBits + 2.
 
-  Definition BoothInStr := STRUCT { "multiplicand" :: Bit NumBits;
-                                    "multiplier" :: Bit NumBits }.
-  Definition BoothOutStr := STRUCT { "high" :: Bit NumBits;
-                                     "low" :: Bit NumBits }.
+  Definition MultInStr := STRUCT { "multiplicand" :: Bit MultNumBits;
+                                   "multiplier" :: Bit MultNumBits }.
+  Definition MultOutStr := STRUCT { "high" :: Bit MultNumBits;
+                                    "low" :: Bit MultNumBits }.
 
-  Definition boothRegister := MethodSig "boothRegister"(Struct BoothInStr): Void.
-  Definition boothGetResult := MethodSig "boothGetResult"(Void): Struct BoothOutStr.
+  Definition multRegister := MethodSig "multRegister"(Struct MultInStr): Void.
+  Definition multGetResult := MethodSig "multGetResult"(Void): Struct MultOutStr.
 
   Definition boothStep' {ty}
              (m_pos m_neg: fullType ty (SyntaxKind (Bit MultBits)))
@@ -55,44 +55,44 @@ Section Multiplier64.
               boothSteps n m_pos m_neg np cont)%kami_action
     end.
 
-  Definition boothMultiplier :=
+  Definition boothMultiplierImpl :=
     MODULE {
       Register "m_pos" : Bit MultBits <- Default
       with Register "m_neg" : Bit MultBits <- Default
       with Register "p" : Bit MultBits <- Default
-      with Register "cnt" : Bit (S LogNumPhases) <- Default
+      with Register "cnt" : Bit (S MultLogNumPhases) <- Default
 
-      with Method "boothRegister"(src: Struct BoothInStr): Void :=
-        LET m : Bit (pred NumBits + 1) <- #src!BoothInStr@."multiplicand";
+      with Method "multRegister"(src: Struct MultInStr): Void :=
+        LET m : Bit (pred MultNumBits + 1) <- #src!MultInStr@."multiplicand";
         LET m_msb <- UniBit (TruncLsb _ 1) #m;
         LET m_neg <- (UniBit (Inv _) #m) + $1;
         LET m_neg_msb <- UniBit (TruncLsb _ 1) #m_neg;
         
         Write "m_pos" : Bit MultBits <-
-          BinBit (Concat _ (S NumBits)) (BinBit (Concat _ _) #m_msb #m) $0;
+          BinBit (Concat _ (S MultNumBits)) (BinBit (Concat _ _) #m_msb #m) $0;
         Write "m_neg" : Bit MultBits <-
-          BinBit (Concat _ (S NumBits)) (BinBit (Concat _ _) #m_neg_msb #m_neg) $0;
+          BinBit (Concat _ (S MultNumBits)) (BinBit (Concat _ _) #m_neg_msb #m_neg) $0;
         
-        LET r <- #src!BoothInStr@."multiplier";
+        LET r <- #src!MultInStr@."multiplier";
         Write "p" : Bit MultBits <-
-          BinBit (Concat (S NumBits) _) $0 (BinBit (Concat _ 1) #r $0);
+          BinBit (Concat (S MultNumBits) _) $0 (BinBit (Concat _ 1) #r $0);
 
-        Write "cnt" : Bit (S LogNumPhases) <- $(NumPhases);
+        Write "cnt" : Bit (S MultLogNumPhases) <- $(MultNumPhases);
         Retv
 
-      with Method "boothGetResult"(): Struct BoothOutStr :=
-        Read cnt : Bit LogNumPhases <- "cnt";
+      with Method "multGetResult"(): Struct MultOutStr :=
+        Read cnt : Bit MultLogNumPhases <- "cnt";
         Assert (#cnt == $0);
 
         Read p : Bit MultBits <- "p";
-        LET high : Bit NumBits <- UniBit (ConstExtract (S NumBits) NumBits 1) #p;
-        LET low : Bit NumBits <- UniBit (ConstExtract 1 NumBits (S NumBits)) #p;
+        LET high : Bit MultNumBits <- UniBit (ConstExtract (S MultNumBits) MultNumBits 1) #p;
+        LET low : Bit MultNumBits <- UniBit (ConstExtract 1 MultNumBits (S MultNumBits)) #p;
 
-        LET ret : Struct BoothOutStr <- STRUCT { "high" ::= $$Default; "low" ::= #low };
+        LET ret : Struct MultOutStr <- STRUCT { "high" ::= $$Default; "low" ::= #low };
         Ret #ret
             
       with Rule "boothStep" :=
-        Read cnt : Bit LogNumPhases <- "cnt";
+        Read cnt : Bit MultLogNumPhases <- "cnt";
         Assert (#cnt != $0);
 
         Read m_pos : Bit MultBits <- "m_pos";
@@ -100,36 +100,157 @@ Section Multiplier64.
         Read p : Bit MultBits <- "p";
 
         boothSteps
-          NumBitsPerPhase m_pos m_neg p
+          MultNumBitsPerPhase m_pos m_neg p
           (fun np => Write "p" <- #np; Retv)
     }.
 
-  Definition boothMultiplierSpec :=
+  Definition multiplierSpec :=
     MODULE {
-      Register "p" : Bit (2 * NumBits) <- Default
+      Register "p" : Bit (2 * MultNumBits) <- Default
 
-      with Method "boothRegister"(src: Struct BoothInStr): Void :=
-        LET m : Bit NumBits <- #src!BoothInStr@."multiplicand";
-        LET m_ext : Bit (2 * NumBits) <- UniBit (SignExtendTrunc _ _) #m;
-        LET r : Bit NumBits <- #src!BoothInStr@."multiplier";
-        LET r_ext : Bit (2 * NumBits) <- UniBit (SignExtendTrunc _ _) #m;
+      with Method "multRegister"(src: Struct MultInStr): Void :=
+        LET m : Bit MultNumBits <- #src!MultInStr@."multiplicand";
+        LET m_ext : Bit (2 * MultNumBits) <- UniBit (SignExtendTrunc _ _) #m;
+        LET r : Bit MultNumBits <- #src!MultInStr@."multiplier";
+        LET r_ext : Bit (2 * MultNumBits) <- UniBit (SignExtendTrunc _ _) #m;
 
         Write "p" <- #m_ext *s #r_ext;
         Retv
 
-      with Method "boothGetResult"(): Struct BoothOutStr :=
-        Read p : Bit (NumBits + NumBits) <- "p";
-        LET high : Bit NumBits <- UniBit (TruncLsb _ _) #p;
-        LET low : Bit NumBits <- UniBit (Trunc _ _) #p;
+      with Method "multGetResult"(): Struct MultOutStr :=
+        Read p : Bit (MultNumBits + MultNumBits) <- "p";
+        LET high : Bit MultNumBits <- UniBit (TruncLsb _ _) #p;
+        LET low : Bit MultNumBits <- UniBit (Trunc _ _) #p;
 
-        LET ret : Struct BoothOutStr <- STRUCT { "high" ::= $$Default; "low" ::= #low };
+        LET ret : Struct MultOutStr <- STRUCT { "high" ::= #high; "low" ::= #low };
         Ret #ret
     }.
 
 End Multiplier64.
 
-Section Divider.
-End Divider.
+(* Below implements non-restoring division for unsigned integers.
+ * Operand signs are re-applied after the division.
+ * NOTE: the rule for operand signs:
+ * when "z = d * q + r", we have "sign(r) = sign(z)" and "|r| < |d|".
+ *)
+Section Divider64.
+  Definition DivLogNumPhases := 3.
+  Definition DivNumBitsPerPhase := 8.
+
+  Local Definition DivNumPhases := wordToNat (wones DivLogNumPhases) + 1.
+  Local Definition DivNumBits := DivNumPhases * DivNumBitsPerPhase.
+  Local Definition DivBits := 2 * DivNumBits + 2.
+
+  Definition DivInStr := STRUCT { "dividend" :: Bit DivNumBits;
+                                  "divisor" :: Bit DivNumBits }.
+  Definition DivOutStr := STRUCT { "quotient" :: Bit DivNumBits;
+                                   "remainder" :: Bit DivNumBits }.
+
+  Definition divRegister := MethodSig "divRegister"(Struct DivInStr): Void.
+  Definition divGetResult := MethodSig "divGetResult"(Void): Struct DivOutStr.
+
+  (*! TODO: optimize this step !*)
+  Definition divStep {ty}
+             (pq: fullType ty (SyntaxKind (Bit (DivNumBits + S DivNumBits))))
+             (dr: fullType ty (SyntaxKind (Bit DivNumBits)))
+    : Expr ty (SyntaxKind (Bit (DivNumBits + S DivNumBits))) :=
+    ((* Step 1: shift *)
+      let pq_shift := BinBit (Sll ((DivNumBits + DivNumBits) + 1) _) #pq $$(natToWord 1 1) in
+      (* Step 2: add or subtract *)
+      let pq_shift_msb := UniBit (TruncLsb _ 1) pq_shift in
+      let pq_shift_p := (IF (pq_shift_msb == $0)
+                         then pq_shift - (BinBit (Concat _ (S DivNumBits)) #dr $0)
+                         else pq_shift + (BinBit (Concat _ (S DivNumBits)) #dr $0)) in
+      (* Step 3: set the quotient bit, which is LSB here *)
+      let pq_shift_p_msb := UniBit (TruncLsb _ 1) pq_shift_p in
+      IF (pq_shift_p_msb == $0)
+      then pq_shift_p + $1
+      else pq_shift_p
+    )%kami_expr.
+  
+  Fixpoint divSteps (cnt: nat)
+           {ty} (pq: fullType ty (SyntaxKind (Bit (DivNumBits + S DivNumBits))))
+           (dr: fullType ty (SyntaxKind (Bit DivNumBits)))
+           (cont: fullType ty (SyntaxKind (Bit (DivNumBits + S DivNumBits))) ->
+                  ActionT ty Void)
+    : ActionT ty Void :=
+    match cnt with
+    | O => (cont pq)%kami_action
+    | S n => (LET npq <- divStep pq dr;
+              divSteps n npq dr cont)%kami_action
+    end.
+
+  Definition nrDividerImpl :=
+    MODULE {
+      Register "pq" : Bit (DivNumBits + S DivNumBits) <- Default
+      with Register "dr" : Bit DivNumBits <- Default
+      with Register "cnt" : Bit (S DivLogNumPhases) <- Default
+
+      with Method "divRegister"(src: Struct DivInStr): Void :=
+        LET dividend : Bit DivNumBits <- #src!DivInStr@."dividend";
+        LET divisor : Bit DivNumBits <- #src!DivInStr@."divisor";
+
+        Write "pq" : Bit (DivNumBits + S DivNumBits) <- UniBit (ZeroExtendTrunc _ _) #dividend;
+        Write "dr" <- #divisor;
+        Write "cnt" : Bit (S DivLogNumPhases) <- $(DivNumPhases);
+        Retv
+
+      with Method "divGetResult"(): Struct DivOutStr :=
+        Read cnt : Bit DivLogNumPhases <- "cnt";
+        Assert (#cnt == $0);
+
+        Read pq : Bit (DivNumBits + S DivNumBits) <- "pq";
+        Read dr : Bit DivNumBits <- "dr";
+        
+        LET p : Bit (DivNumBits + 1) <- UniBit (TruncLsb _ _) #pq;
+        LET q : Bit DivNumBits <- UniBit (Trunc _ _) #pq;
+
+        LET p_msb <- UniBit (TruncLsb _ 1) #p;
+        LET p_ls : Bit DivNumBits <- UniBit (Trunc _ _) #p;
+        LET rem : Bit DivNumBits <- (IF (#p_msb == $0)
+                                     then #p_ls
+                                     else #p_ls + #dr);
+        
+        LET ret : Struct DivOutStr <- STRUCT { "quotient" ::= #q;
+                                               "remainder" ::= #rem
+                                             };
+        Ret #ret
+            
+      with Rule "divStep" :=
+        Read cnt : Bit DivLogNumPhases <- "cnt";
+        Assert (#cnt != $0);
+
+        Read pq : Bit (DivNumBits + S DivNumBits) <- "pq";
+        Read dr : Bit DivNumBits <- "dr";
+        
+        divSteps
+          DivNumBitsPerPhase pq dr
+          (fun npq => Write "pq" <- #npq; Retv)
+    }.
+
+  Definition dividerSpec :=
+    MODULE {
+      Register "quotient" : Bit DivNumBits <- Default
+      with Register "remainder" : Bit DivNumBits <- Default
+                         
+      with Method "DivRegister"(src: Struct DivInStr): Void :=
+        LET m : Bit DivNumBits <- #src!DivInStr@."dividend";
+        LET r : Bit DivNumBits <- #src!DivInStr@."divisor";
+
+        Write "quotient" <- #m /s #r;
+        Write "remainder" <- BinBit (Rem _ SignSS) #m #r;
+        Retv
+
+      with Method "DivGetResult"(): Struct DivOutStr :=
+        Read quotient : Bit DivNumBits <- "quotient";
+        Read remainder : Bit DivNumBits <- "remainder";
+
+        LET ret : Struct DivOutStr <- STRUCT { "quotient" ::= #quotient;
+                                               "remainder" ::= #remainder };
+        Ret #ret
+    }.
+  
+End Divider64.
 
 Section Processor.
   Variable NumDataBytes: nat.
